90bd6ffc6ba03c6214420fa5bf796b24ef1140a6
Lestrous <shtol.leonid@mail.ru>
2021-10-18 13:29:54 +0500

    Merge remote-tracking branch 'origin/week-4' into week-4

---
+++ public_html/result4/index.js
@@ -0,0 +1,25 @@
+import { Server } from 'http';
+
+const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept,Access-Control-Allow-Headers'
+};
+
+Server((req, res) => {
+    let requestBody = '';
+
+    req.on('data', chunk => {
+        requestBody += chunk;
+    });
+
+    req.on('end', () => {
+        res.writeHead(200, { 'Content-Type': 'application/json; charset=UTF-8', ...CORS });
+
+        const jsonRes = {"message": "day108", "x-result": req['headers']['x-test'], "x-body": requestBody};
+        const str = JSON.stringify(jsonRes);
+
+        res.end( str );
+    });
+})
+    .listen(4321);
